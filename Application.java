c5f55bbb0eca28adcd7343ef8fb3a85f045a10aa
niall <niall@10.10.28.197>
2023-09-05 20:56:20 -0400

    demo

---
+++ Application.java
@@ -0,0 +1,26 @@
+
+
+import Genericbag.Item;
+import Genericbag.SmallBag;
+
+public class Application {
+
+	public static void main(String[] args) 
+	{
+		Item<String> sb1 = new Item<String>("Saaki Vishnumolakala");
+		
+		Item<Integer> sb2 = new Item<Integer>(17);
+		
+		
+		
+		
+		SmallBag<Item> sb3 = new SmallBag<Item>();
+		
+		sb3.setSmallBag(sb1);
+		System.out.println(sb3.getSmallBag().getItem());
+		
+		sb3.setSmallBag(sb2);
+		System.out.println(sb3.getSmallBag().getItem());
+	}
+
+}
---
+++ SmallBag.java
@@ -0,0 +1,20 @@
+
+
+public class SmallBag<T>
+{
+	private T e;
+	
+	public SmallBag() {
+		
+	}
+
+	public void setSmallBag(T e)
+	{
+		this.e = e;
+	}
+	
+	public T getSmallBag()
+	{
+		return e;
+	}
+}
